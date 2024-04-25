f3642c1e0bf54d55ddbfca3b6b41e91870b223a0
Топалов Василий <topalek@mail.ru>
2024-04-25 18:17:18 +0300

    flash

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -60,9 +60,12 @@ class AuthController extends Controller
         $status = Password::sendResetLink(
             $request->only('email')
         );
-        return $status === Password::RESET_LINK_SENT
-            ? back()->with(['message' => __($status)])
-            : back()->withErrors(['email' => __($status)]);
+
+        if ($status === Password::RESET_LINK_SENT) {
+            flash()->info(__($status));
+            return back();
+        }
+        return back()->withErrors(['email' => __($status)]);
     }
 
     public function resetPassword($token)
@@ -85,18 +88,20 @@ class AuthController extends Controller
             }
         );
 
-        return $status === Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('message', __($status))
-            : back()->withErrors(['email' => [__($status)]]);
+        if ($status === Password::PASSWORD_RESET) {
+            flash()->info(__($status));
+            return back();
+        }
+        return back()->withErrors(['email' => __($status)]);
     }
 
     public function logout()
     {
         auth()->logout();
 
-        request()->session()->invalidate();
+        session()->invalidate();
 
-        request()->session()->regenerateToken();
+        session()->regenerateToken();
 
         return redirect('/');
     }
---
+++ config/flash.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'info'  => 'bg-purple text-white text-center',
+    'alert' => 'bg-red-400 text-white text-center',
+];
