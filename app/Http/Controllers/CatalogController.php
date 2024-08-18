4157c29873078a690771f2c73012890d2f0206f7
Топалов Василий <topalek@mail.ru>
2024-08-18 10:36:06 +0300

    view-models

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,24 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\View\ViewModels\CatalogViewModel;
 use Domain\Catalog\Models\Category;
-use Domain\Product\Models\Product;
 
 class CatalogController extends Controller
 {
     public function __invoke(?Category $category)
     {
-        $categories = Category::query()->select(['id', 'title', 'slug'])->has('products')->get();
-        $products = Product::query()
-            ->with('brand')
-            ->select(['id', 'brand_id', 'title', 'slug', 'price', 'thumbnail', 'json_properties'])
-            ->search()
-            ->withCategory($category)
-                           ->filtered()
-                           ->sorted()
-                           ->paginate(6)
-        ;
-
-        return view('catalog.index', compact('products', 'categories', 'category'));
+        return view('catalog.index', new CatalogViewModel($category));
     }
 }
